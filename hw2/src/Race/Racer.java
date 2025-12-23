1cefc38bc13afdec1460b4fd9ac518cb5e65f124
ilan <ilankot76@gmail.com>
2025-12-23 21:50:08 +0200

    fwa

--- hw2/src/Race/Racer.java
+++ hw2/src/Race/Racer.java
@@ -3,7 +3,7 @@ package Race;
 import java.util.Scanner;
 
 public class Racer implements Runnable {
-
+    Scanner scanner = new Scanner(System.in);
     static private int globalid = 1;
     private int id;
     private int speed;
@@ -13,7 +13,7 @@ public class Racer implements Runnable {
 
         this.track = track;
         this.id = globalid++;
-        Scanner scanner = new Scanner(System.in);
+        ;
 
         while (speed < 1 || speed > 10) {
             System.out.println("Invalid speed for racer " + id + ". Please enter a speed between 1 and 10:");
@@ -27,29 +27,30 @@ public class Racer implements Runnable {
         go();
     }
 
-   public void go() {
-    Thread.currentThread().setPriority(this.speed);
+    public void go() {
+        Thread.currentThread().setPriority(this.speed);
 
-    for (int i = 1; i <= 100; i++) {
-        System.out.println("Runner " + id + " ran " + i + " meters");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Runner " + id + " ran " + i + " meters");
 
-        if (i == 100) {
-            synchronized (track) {
-                track.finishedRacers++;          // increment first
-                int place = track.finishedRacers;
+            if (i == 100) {
+                synchronized (track) {
+                    System.out.println("Runner " + id + " reached 100 meters!");
+                    track.finishedRacers++; // increment first
+                    int place = track.finishedRacers;
 
-                if (place == 1) {
-                    System.out.println("Runner " + id + " finished 1st!");
-                } else if (place == 2) {
-                    System.out.println("Runner " + id + " finished 2nd!");
-                } else if (place == 3) {
-                    System.out.println("Runner " + id + " finished 3rd!");
-                } else {
-                    System.out.println("Runner " + id + " finished " + place + "th!");
+                    if (place == 1) {
+                        System.out.println("Runner " + id + " finished 1st!");
+                    } else if (place == 2) {
+                        System.out.println("Runner " + id + " finished 2nd!");
+                    } else if (place == 3) {
+                        System.out.println("Runner " + id + " finished 3rd!");
+                    } else {
+                        System.out.println("Runner " + id + " finished " + place + "th!");
+                    }
                 }
             }
         }
     }
-}
 
 }
