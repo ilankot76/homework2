1ecd5c1c1241d4fd30f768616073e6c07e32024a
ilan <ilankot76@gmail.com>
2025-12-25 03:47:47 +0200

    ffes

--- Hw2T3/src/XO/App.java
+++ Hw2T3/src/XO/App.java
@@ -1,5 +1,4 @@
 package XO;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ Hw2T3/src/XO/Game.java
@@ -0,0 +1,5 @@
+package XO;
+
+public class Game {
+
+}
